43c08c68caae629c0de50b2b387948a4c099bd4f
vouillon <vouillon@users.noreply.github.com>
2026-07-09 10:55:03 +0000

    deploy: d763401a54e15736726dd55c13315738c1fff569

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c9821482.js',
+                path_to_root + 'searchindex-9629224b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
